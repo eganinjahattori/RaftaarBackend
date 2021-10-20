29a367c8cea39c02c82abcfa32950e0be0d4bef9
RAJAT UPADHYAY ENCY <rajat.upadhyay@encycg.com>
2021-10-21 00:35:10 +0530

    added the route to fetch all contact and bookings, store new bookings

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ const contactSchema = mongoose.Schema({
 
 const contactModel = mongoose.model('contact', contactSchema);
 
+const bookTableSchema = mongoose.Schema({
+    Name: { type: String },
+    Email: { type: String },
+    Phone: { type: Number },
+    Date: { type: String },
+    Time: { type: String },
+    People: { type: Number },
+    Message: { type: String },
+});
+
+const bookTableModel = mongoose.model('table', bookTableSchema);
+
 var mail = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -54,7 +66,7 @@ app.post('/contact', async (req, res) => {
 
         var mailOptions = {
             from: 'eganinjahattori@gmail.com',
-            to: 'rajatis1999@gmail.com',
+            to: ['rajat.program@gmail.com', 'Raftaarloungedelhi@gmail.com'],
             subject: 'New Query Received in Contact Us Form (www.raftaarlounge.com)',
             text: `Name: ${name} \nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`
         };
@@ -65,7 +77,7 @@ app.post('/contact', async (req, res) => {
             } else {
                 console.log('Email sent: ' + info.response);
             }
-        }); 
+        });
 
         return res.status(200).send("We received your data");
     }
@@ -73,6 +85,71 @@ app.post('/contact', async (req, res) => {
         console.log(err);
         return res.status(500).send("There was some error in the server. Please contact your administrator.");
     }
+});
+
+app.post('/bookTable', async (req, res) => {
+    try {
+        const { name, email, phone, date, time, people, message } = req.body;
+        console.log(name, email, phone, date, time, people, message);
+
+        var newTable = new bookTableModel({
+            Name: name, Email: email, Phone: phone,
+            Date: date,
+            Time: time,
+            People: people,
+            Message: message
+        });
+        newTable.save((err, record) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            console.log(record);
+        });
+
+        var mailOptions = {
+            from: 'eganinjahattori@gmail.com',
+            to: ['rajat.program@gmail.com', 'Raftaarloungedelhi@gmail.com'],
+            subject: 'New Book Received in Book Table Form (www.raftaarlounge.com)',
+            text: `Name: ${name} \nEmail: ${email}\nPhone: ${phone}\nDate: ${date}\nTime: ${time}\nPeople: ${people}\nMessage: ${message}`
+        };
+
+        mail.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+        return res.status(200).send("We received your data");
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).send("There was some error in the server. Please contact your administrator.");
+    }
+});
+
+app.get('/all/query', async(req, res) => {
+    contactModel.find({}, function (err, records) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send("Some error occured. Please contact your administrator.");
+        } else {
+            return res.status(200).send({data: records});
+        }
+    })
+});
+
+app.get('/all/booking', async(req, res) => {
+    bookTableModel.find({}, function (err, records) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send("Some error occured. Please contact your administrator.");
+        } else {
+            return res.status(200).send({data: records});
+        }
+    })
 });
 
 app.listen(4040, () => {
